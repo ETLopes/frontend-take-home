3c38d80bf48d2b2f25b8bfdecfa470ec6950b669
Eduardo Lopes <eduardotorreslopes@gmail.com>
2025-04-21 18:23:17 -0300

    fix bottom sheet background color

--- src/common/components/BottomSheet.tsx
+++ src/common/components/BottomSheet.tsx
@@ -1,10 +1,18 @@
 import BottomSheetComponent, { BottomSheetProps } from "@gorhom/bottom-sheet"
 import { forwardRef } from "react"
+import { useTheme } from "../theme/ThemeContext"
 
 export type BottomSheet = BottomSheetComponent
 
 export const BottomSheet = forwardRef<BottomSheetComponent, BottomSheetProps>(
 	function BottomSheet(props, ref) {
-		return <BottomSheetComponent ref={ref} {...props} />
+		const { colors } = useTheme()
+		return (
+			<BottomSheetComponent
+				ref={ref}
+				{...props}
+				backgroundStyle={{ backgroundColor: colors.layer.solid.light }}
+			/>
+		)
 	}
 )
